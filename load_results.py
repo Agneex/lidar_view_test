aa1d0ad85a87138db8512c8c1002694e0662384c
oriojas <oscarriojas@gmail.com>
2024-11-19 00:18:25 -0500

    feat/ improve cluster

--- load_results.py
+++ load_results.py
@@ -1,5 +1,5 @@
 from point_cloud import visualize_clustered_parquet
 
-data = 'data/clustered_points.parquet'
+data = 'data/clustered_points_dbscan.parquet'
 
 visualize_clustered_parquet(data, p_size=2, show_noise=True)
